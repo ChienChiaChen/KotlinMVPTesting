2671ead87a87c6d8f82742713ef94c9665554515
ChiaChen <s124202468@gmail.com>
2018-08-13 12:12:35 +0800

    Adding GetUsers.

---
+++ app/src/main/java/com/example/chiachen/ktmvp/domain/interactors/GetUsers.kt
@@ -0,0 +1,16 @@
+package com.example.chiachen.ktmvp.domain.interactors
+
+import com.example.chiachen.ktmvp.models.entities.UserListModel
+import com.example.chiachen.ktmvp.models.repositories.UserRepository
+import com.example.chiachen.ktmvp.presentation.view.viewmodels.UserViewModel
+import io.reactivex.Single
+
+class GetUsers(private val userRepository: UserRepository) {
+    fun execute(page: Int, forced: Boolean): Single<List<UserViewModel>> {
+        val userList = userRepository.getUsers(page, forced)
+        return userList.map { userListModel: UserListModel? ->
+            val items = userListModel?.items ?: emptyList()
+            items.map { UserViewModel(it.userId, it.display, it.reputation, it.profile) }
+        }
+    }
+}
