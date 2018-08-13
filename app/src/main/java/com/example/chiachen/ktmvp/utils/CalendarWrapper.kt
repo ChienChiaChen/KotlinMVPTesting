17c5a1361677a18a87d919f923e982808b0eff14
ChiaChen <s124202468@gmail.com>
2018-08-13 11:13:46 +0800

    Adding helper for calendar, connection and preference.

---
+++ app/src/main/java/com/example/chiachen/ktmvp/utils/CalendarWrapper.kt
@@ -0,0 +1,7 @@
+package com.example.chiachen.ktmvp.utils
+
+import java.util.*
+
+class CalendarWrapper {
+    fun getCurrentTimeInMillis() = Calendar.getInstance().timeInMillis
+}
---
+++ app/src/main/java/com/example/chiachen/ktmvp/utils/ConnectionHelper.kt
@@ -0,0 +1,14 @@
+package com.example.chiachen.ktmvp.utils
+
+import android.content.Context
+import android.net.ConnectivityManager
+
+class ConnectionHelper(private val context: Context) {
+
+    fun isOnline(): Boolean {
+        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val netInfo = connectivityManager.activeNetworkInfo
+        return netInfo != null && netInfo.isConnected
+    }
+
+}
