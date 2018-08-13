b2ca4baf6c2302911ea3bcdabbec8f61d9378186
ChiaChen <s124202468@gmail.com>
2018-08-13 12:21:38 +0800

    Refine application component.

--- app/src/main/java/com/example/chiachen/ktmvp/di/components/ApplicationComponent.kt
+++ app/src/main/java/com/example/chiachen/ktmvp/di/components/ApplicationComponent.kt
@@ -2,6 +2,7 @@ package com.example.chiachen.ktmvp.di.components
 
 import com.example.chiachen.ktmvp.MyApp
 import com.example.chiachen.ktmvp.di.modules.ApplicationModule
+import com.example.chiachen.ktmvp.di.modules.MainActivityModule
 import dagger.Component
 import javax.inject.Singleton
 
@@ -9,4 +10,5 @@ import javax.inject.Singleton
 @Component(modules = arrayOf(ApplicationModule::class))
 interface ApplicationComponent {
     fun inject(application: MyApp)
+    fun inject(mainActivityModule: MainActivityModule): MainActivityComponent
 }
